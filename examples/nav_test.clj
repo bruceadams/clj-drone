a68d4d15917cf16e75931895e6daaa9653334364
Bruce Adams <bruce.adams@acm.org>
2013-09-08 21:59:33 -0400

    nav_test enhanced
    Log more stuff, especially the watchdog.
    Add flat-trim and some debug messages.

--- examples/nav_test.clj
+++ examples/nav_test.clj
@@ -4,14 +4,22 @@
 
 ;; logging is configured to go to the logs/drone.log file
 
-(set-log-data [:seq-num :flying :battery-percent :con
+(println (java.util.Date.))
+(set-log-data [:seq-num :flying :battery-percent
                :control-state :roll :pitch :yaw
-                :velocity-x :velocity-y :velocity-z :com-watchdog])
+               :velocity-x :velocity-y :velocity-z])
 (drone-initialize)
-
+(println "init-navdata" (java.util.Date.))
 (drone-init-navdata)
+(drone :flat-trim)
+(println "ask for take off" (java.util.Date.))
 (drone :take-off)
-(Thread/sleep 10000)
+(Thread/sleep 6000)
+(println "ask for landing" (java.util.Date.))
 (drone :land)
+(Thread/sleep 3000)
+(println "end-navstream" (java.util.Date.))
 (end-navstream)
+(println (java.util.Date.))
 
+;; (drone :emergency)
